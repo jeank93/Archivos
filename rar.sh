dfc5ac217a7b8e15841c808638bd1c26cf4db18d
jeank93 <jcarlos1993@hotmail.com>
2020-11-08 07:53:22 -0600

    Add files via upload

---
+++ rar.sh
@@ -0,0 +1,14 @@
+VERSION=5.9.1
+wget -qO- https://www.rarlab.com/rar/rarlinux-x64-${VERSION}.tar.gz | tar -xz
+cd rar
+
+chmod +x rar unrar
+sudo mv rar unrar /usr/bin
+sudo mv rarfiles.lst /etc
+
+wget -O rarreg.key https://pastebin.com/raw/Ed7iQSPm
+sudo mv default.sfx rarreg.key /usr/local/lib
+
+cd ..
+rm -r rar
+exec bash
