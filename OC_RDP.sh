3ee048b275538f36d88b4691a54becfcf5e53fb6
jeank93 <jcarlos1993@hotmail.com>
2020-12-11 12:22:55 -0600

    Update OC_RDP.sh

--- OC_RDP.sh
+++ OC_RDP.sh
@@ -59,10 +59,10 @@ printf "$g$b    Installing Desktop Environment $endc$enda" >&2
 {
     sudo DEBIAN_FRONTEND=noninteractive \
         apt install --assume-yes xfce4 desktop-base xfce4-whiskermenu-plugin
-	git clone https://github.com/evandrocoan/MyLinuxSettings.git /home/user/Downloads/MyLinuxSettings
-	rsync -r -t -v -s /home/user/Downloads/MyLinuxSettings/ /home/user/
-	git config --global core.excludesfile /home/user/.gitignore_global
-	sudo chmod 600 /home/user/.ssh/config
+    git clone https://github.com/evandrocoan/MyLinuxSettings.git /home/user/Downloads/MyLinuxSettings
+    rsync -r -t -v -s /home/user/Downloads/MyLinuxSettings/ /home/user/
+    git config --global core.excludesfile /home/user/.gitignore_global
+#sudo chmod 600 /home/user/.ssh/config
     sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
     sudo apt install --assume-yes xscreensaver
     sudo systemctl disable lightdm.service
